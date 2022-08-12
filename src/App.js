a88ca54aa5862adbb239f6ead29cef00c4cc428e
dgregpm <dgregpm@gmail.com>
2022-08-11 22:33:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import React from 'react';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
 
 function App() {
   return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+    
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-3xl font-bold underline">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -19,6 +37,7 @@ function App() {
         </a>
       </header>
     </div>
+    */
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,38 @@
+// src/data.js
+
+export const projects = [
+    {
+      title: "Awesome App",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "http://placeimg.com/640/480/tech",
+      link: "https://gregorypd.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Node",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "http://placeimg.com/640/480/tech",
+      link: "https://website.com",
+    },
+    {
+      title: "Awesome App",
+      subtitle: "React and Node",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "http://placeimg.com/640/480/tech",
+      link: "https://website.com",
+    },
+    {
+      title: "Awesome App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "http://placeimg.com/640/480/tech",
+      link: "https://website.com",
+    },
+  ];
+
+  export const skills = ['React-js', 'Node', 'Javascript', 'MongoDb', 'Express', 'Tailwind-CSS'];
