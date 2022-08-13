61b2773519e72b13505e5503a8329e62dec8b110
dgregpm <dgregpm@gmail.com>
2022-08-12 21:23:11 -0400

    fixed responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,12 @@ import logo from './../logo.svg';
 function About() {
     return (
         <section id='about'>
-            <div className="container mx-auto flex px-10 py-20 md:flex-row items-center border-solid border-2 border-purple-500 h-screen">
-                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center border-solid border-2 border-purple-500">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                        Hi, I'm Gregory.
-                        <br className="hidden lg:inline-block" />
-                        I love to build amazing apps.
+                        <p className="m-0">Hi, I'm Gregory.</p>
+                        
+                        <p className="m-0">I'm a Full Stack Software Engineer.</p>
                     </h1>
                     <p className="mb-8 leading-relaxed">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui laborum quasi, incidunt dolore iste nostrum cupiditate voluptas? Laborum, voluptas natus?
@@ -27,9 +27,9 @@ function About() {
                         </a>
                     </div>
                 </div>
-                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 border-solid border-2 border-purple-500">
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                     <img
-                        className="object-cover object-center rounded"
+                        className="min-w-fit object-cover object-center rounded]"
                         alt="hero"
                         src={logo}
                     />
@@ -71,7 +71,7 @@ function About2() {
             <img
               className="object-cover object-center rounded"
               alt="hero"
-              src="./coding.svg"
+              src={logo}
             />
           </div>
         </div>
