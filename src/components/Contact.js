e0acfc6c73238d45f7d5593b4512b6b2de00fc21
dgregpm <dgregpm@gmail.com>
2022-11-26 12:45:19 -0500

    Fixed some css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,7 +28,7 @@ function Contact() {
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <iframe
+          {/* <iframe
             width="100%"
             height="100%"
             title="map"
@@ -38,28 +38,55 @@ function Contact() {
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
             src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-          />
+          /> */}
+
+          {/* <iframe 
+          title="map"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114726.80681455339!2d-80.32778889999999!3d26.0265953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9a698bf8cc6ab%3A0x9be2cf42d1963635!2sPembroke%20Pines%2C%20FL!5e0!3m2!1sen!2sus!4v1669483405099!5m2!1sen!2sus" 
+          width="100%" 
+          height="100%" 
+          style={{wtf: "border:0"}} 
+          allowfullscreen="" 
+          loading="lazy" 
+          referrerpolicy="no-referrer-when-downgrade">
+          </iframe> */}
+          
+          <iframe 
+          title="map"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114726.80681455339!2d-80.32778889999999!3d26.0265953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9a698bf8cc6ab%3A0x9be2cf42d1963635!2sPembroke%20Pines%2C%20FL!5e0!3m2!1sen!2sus!4v1669483794899!5m2!1sen!2sus" 
+          width="100%" 
+          height="100%" 
+          style={{ filter: "opacity(0.7)" }}
+          className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+          allowfullscreen="" 
+          loading="lazy" 
+          referrerpolicy="no-referrer-when-downgrade">
+          </iframe>
+
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 ADDRESS
               </h2>
               <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
+                Pembroke Pines <br />
+                Florida
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
-              </a>
+              <span className="text-indigo-400 leading-relaxed">
+                dgregpm@gmail.com
+              </span>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">954-367-9221</p>
             </div>
           </div>
         </div>
